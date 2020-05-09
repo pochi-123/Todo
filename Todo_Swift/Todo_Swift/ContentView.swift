1e01e7b231f66e8d5f32d5e4649063f574dfaedc
akane <mfmf.mfmf.mfmf.pu@gmail.com>
2020-05-09 23:22:20 +0900

    プロジェクト作成

---
+++ Todo_Swift/Todo_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo_Swift
+//
+//  Created by Akane Yasuda on 2020/04/28.
+//  Copyright © 2020 Pochi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
